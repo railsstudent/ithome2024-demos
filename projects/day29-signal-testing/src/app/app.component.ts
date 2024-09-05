62670867fb5f33a4709c311b6524dc065fa9f82f
Connie Leung <cleung334@gmail.com>
2024-09-05 08:58:45 +0800

    feat: create simple couter service

--- projects/day29-signal-testing/src/app/app.component.ts
+++ projects/day29-signal-testing/src/app/app.component.ts
@@ -1,18 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   template: `
     <h1>Hello, {{ title }}</h1>
     <div>
-
+      <p>Value: {{ this.appService.value() }}</p>
+      <button id="increase" (click)="increase(1)">Add 1</button>
+      <button id="increase2" (click)="increase(2)">Add 2</button>
+      <button id="decrease" (click)="decrease(1)">Decrease</button>
+      <button id="decrease2" (click)="decrease(2)">Decrease 2</button>
+      <button id="reset">Reset</button>  
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'day29-signal-testing';
+
+  appService = inject(AppService);
+
+  increase(num = 1) {
+    this.appService.increase(num);
+  }
+
+  decrease(num = 1) {
+    this.appService.decrease(num);
+  }
+
+  reset() {
+    this.appService.reset();
+  }
 }
--- projects/day29-signal-testing/src/app/app.service.ts
+++ projects/day29-signal-testing/src/app/app.service.ts
@@ -1,20 +1,22 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
-  counter = signal(0);
+  #counter = signal(0);
+
+  value = computed(() => this.#counter());
 
   increase(num = 1) {
-    this.counter.update((prev) => prev + num);
+    this.#counter.update((prev) => prev + num);
   }
 
   decrease(num = 1) {
-    this.counter.update((prev) => prev - num);
+    this.#counter.update((prev) => prev - num);
   }
 
   reset() {
-    this.counter.set(0);
+    this.#counter.set(0);
   }
 }
