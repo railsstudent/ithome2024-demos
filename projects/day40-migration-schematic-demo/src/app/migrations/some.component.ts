cc916825add2e707ee761956df37a84180779afe
Connie Leung <cleung334@gmail.com>
2024-11-02 01:10:48 +0800

    refactor: convert numSub to signal and double to an output

--- projects/day40-migration-schematic-demo/src/app/migrations/some.component.ts
+++ projects/day40-migration-schematic-demo/src/app/migrations/some.component.ts
@@ -1,5 +1,5 @@
 import { outputFromObservable } from '@angular/core/rxjs-interop';
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BehaviorSubject, map } from 'rxjs';
 
@@ -12,10 +12,10 @@ import { BehaviorSubject, map } from 'rxjs';
     <div>
       <p>bgColor: {{ bgColor() }}</p>
       <p>name: {{ name() }}</p>
-      @let n = numSub.getValue();
+      @let n = num();
       <p>num: {{ n }}</p>
       <div>
-        Num: <input type="number" [ngModel]="n" (ngModelChange)="numSub.next($event)" />
+        Num: <input type="number" [ngModel]="n" (ngModelChange)="updateNum($event)" />
       </div>
       <button (click)="triple.emit(n * 3)" >Show triple in the parent</button>
       <button (click)="powerXBy3.emit(n * n * n)" >Show cube in the parent</button>
@@ -31,6 +31,11 @@ export class SomeComponent {
   readonly triple = output<number>();
   readonly powerXBy3 = output<number>({ alias: 'cube' });
 
-  numSub = new BehaviorSubject<number>(2);  
-  double = outputFromObservable(this.numSub.pipe(map((n) => n * 2)));
+  num = signal(2);
+  double = output<number>();
+
+  updateNum(value: number) {
+    this.num.set(value); 
+    this.double.emit(value * 2);
+  }
 }
