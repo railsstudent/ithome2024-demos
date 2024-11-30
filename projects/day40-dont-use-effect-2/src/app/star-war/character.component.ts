856e16e5be8a71b3ee8c2b0abb7b5ece4f2c911f
Connie Leung <cleung334@gmail.com>
2024-11-30 09:01:34 +0800

    refactor: computed with signal with linkedSignal

--- projects/day40-dont-use-effect-2/src/app/star-war/character.component.ts
+++ projects/day40-dont-use-effect-2/src/app/star-war/character.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, linkedSignal, signal, WritableSignal } from '@angular/core';
 import { map, mergeMap, catchError, of, forkJoin, switchMap, Observable } from 'rxjs';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
@@ -66,37 +66,14 @@ function getPersonMovies(http: HttpClient) {
       <button (click)="updateId(-1)">-1</button>
       <button (click)="updateId(1)">+1</button>
       <button (click)="updateId(2)">+2</button>
-      <input type="number" [ngModel]="state().searchId()" (ngModelChange)="syncId($event)" />
+      <input type="number" [(ngModel)]="searchId" />
     </div>
-    SearchId: {{ state().searchId() }}
+    SearchId: {{ searchId() }}
   `,
-  styles: `
-    :host {
-      display: block;
-      font-size: 1.5rem;
-      padding: 1rem;
-      --main-font-size: 1.25rem;
-    }
-
-    .border {
-      border: 1px solid black; 
-      border-radius: 0.5rem; 
-      padding: 1rem; 
-      margin-bottom: 1rem;
-
-      color: var(--main-color);
-      font-size: var(--main-font-size);
-    }
-
-    .container {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: center;
-    }
-  `,
+  styleUrl: './character.component.css',
   host: {
-    '[style.--main-color]': 'state().rgb',
-    '[style.--main-font-size]': 'state().fontSize',
+    '[style.--main-color]': 'state2().rgb',
+    '[style.--main-font-size]': 'state2().fontSize',
   },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -117,22 +94,29 @@ export class CharacterComponent {
   state = computed(() => {
     const result = this.#personMovies();
     return { 
-      fontSize: this.id() % 2 === 0 ? '1.25rem' : '1.75rem',
-      rgb: generateRGBCode(),
       person: signal(result && result.length > 0 ? result[0] : undefined),
       films: signal(result && result.length > 1 ? result.slice(1): []),
-      searchId: signal(this.id()),
     };
   }); 
 
+  searchId = linkedSignal<WritableSignal<number>, number>({
+    source: () => this.id,
+    computation: (source, previous) => { 
+      const id = source();
+      if (!previous) {
+        return id;
+      }
+
+      return (id >= this.min && id <= this.max) ? id : previous.value;
+    }
+  });
+
+  state2 = computed(() => ({ 
+    fontSize: this.id() % 2 === 0 ? '1.25rem' : '1.75rem',
+    rgb: generateRGBCode(),
+  }));
+
   updateId(delta: number) {
     this.id.update((value) => Math.min(this.max, Math.max(this.min, value + delta)));
-  }
-  
-  syncId(id: number) {
-    if (id >= this.min && id <= this.max) {
-      this.state().searchId.set(id);
-      this.id.set(id);
-    }
   }
 }
