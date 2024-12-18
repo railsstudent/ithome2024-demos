4082da96de3144a939de78422933c6fe872cd8b3
Connie Leung <cleung334@gmail.com>
2024-12-18 23:33:12 +0800

    chore: delete boilerplates

--- projects/day41-after-render-effect/src/app/app.component.ts
+++ projects/day41-after-render-effect/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [],
+  template: `<div>Testing</div>`,
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'day41-afterRenderEffect';
