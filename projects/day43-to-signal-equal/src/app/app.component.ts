a410331e118c88371ebf70940888074852d83035
Connie Leung <cleung334@gmail.com>
2025-01-01 00:02:03 +0800

    chore: add global styles

--- projects/day43-to-signal-equal/src/app/app.component.ts
+++ projects/day43-to-signal-equal/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: '<div>It works</div>',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'day43-toSignal-equal';
