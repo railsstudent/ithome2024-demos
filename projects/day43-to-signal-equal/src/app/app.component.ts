ade4691f719b4e77b70b263be008267fc76d9eeb
Connie Leung <cleung334@gmail.com>
2025-01-03 23:20:25 +0800

    chore: delete unused import

--- projects/day43-to-signal-equal/src/app/app.component.ts
+++ projects/day43-to-signal-equal/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   template: '<div>It works</div>',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
